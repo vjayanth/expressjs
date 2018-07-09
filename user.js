7986e32e2a3a3965915cfc48a74ce6aa5bda67ee
jayanth <vjayanth1291@gmail.com>
2018-07-09 23:17:56 +0530

    generated jwt token

--- user.js
+++ user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const validate = require("./validate");
 const db = require("./db");
 const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
 
 router.post("/signup", async function(req, res) {
   if (!validate.validateEmail(req.body.emailId)) {
@@ -21,21 +22,54 @@ router.post("/signup", async function(req, res) {
     res.status(400);
     res.send({ msg: "Please Enter valid Name" });
   }
-  req.body.password = await hashPassword(req.body);
+  req.body.password = await bcrypt.hashSync(req.body.password, 10);
   const user = await createUser(res, req.body);
   res.send(user);
 });
-function hashPassword(body) {
+
+router.post("/login", async function(req, res) {
+  if (!validate.validateEmail(req.body.emailId)) {
+    res.status(400);
+    res.send({ msg: "Please enter valid email ID" });
+  }
+  const user = await findUser(req.body);
+  if (user.length == 0) {
+    res.send({ msg: "No User found" });
+    res.status(404);
+  } else {
+    const result = bcrypt.compareSync(req.body.password, user[0].password);
+    if (result) {
+      const payload = {
+        user: user[0].emailId,
+        name: user[0].name
+      };
+      console.log(payload);
+      const token = jwt.sign(payload, "firstApplication", {
+        expiresIn: 5
+      });
+
+      res.send({ msg: "User Authentication Successful", token: token });
+    } else {
+      res.status(401);
+      res.send({ msg: "User Authentication Failed" });
+    }
+  }
+});
+
+
+
+function findUser(body) {
   return new Promise((resolve, reject) => {
-    bcrypt.hash(body.password, 10, function(err, hash) {
-      if (err) {
-        console.log(err);
+    db.query(
+      "select * from user where user.emailId = ?",
+      body.emailId,
+      function(err, results) {
+        resolve(results);
       }
-      console.log(hash);
-      resolve(hash);
-    });
+    );
   });
 }
+
 function createUser(res, body) {
   return new Promise((resolve, reject) => {
     db.query("INSERT INTO user SET ? ", body, function(err, result) {
